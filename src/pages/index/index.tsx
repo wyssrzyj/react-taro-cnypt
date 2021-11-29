0b9fd7a42434cbc74f1b8ba9b330e32dc08fca3f
金阿敏 <13587760358@163.com>
2021-11-29 14:47:40 +0800

    f

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -229,23 +229,6 @@ const Home = () => {
     setPageNum(1)
   }
 
-  const toEntry = () => {
-    Taro.navigateTo({
-      url: '/pages/orderIssueEntry/index'
-    })
-  }
-  const toEntry1 = () => {
-    Taro.navigateTo({
-      url: '/pages/factoryEntry/index'
-    })
-  }
-  const toEntry2 = () => {
-    Taro.navigateTo({
-      url: '/pages/publish/index'
-    })
-  }
-
-  //
   return (
     <View className={styles.container}>
       <Navbar>
@@ -270,10 +253,6 @@ const Home = () => {
           </Text>
         </View>
       </Navbar>
-
-      <Button onClick={toEntry}>发单商入驻</Button>
-      <Button onClick={toEntry1}>加工厂入驻</Button>
-      <Button onClick={toEntry2}>发单</Button>
 
       <View className={isIOS ? styles.IOSContent : styles.content}>
         <Swiper
