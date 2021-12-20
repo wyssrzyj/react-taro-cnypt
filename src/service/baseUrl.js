6aab43daca2531525db370cef2ff8ac222307985
luyingjie <lyj1974451304@163.com>
2021-12-20 17:23:12 +0800

    完成了轮播图的修改

--- src/service/baseUrl.js
+++ src/service/baseUrl.js
@@ -6,9 +6,9 @@ const getBaseUrl = url => {
     BASE_URL = 'http://192.168.69.130:8888'
   } else if (process.env.NODE_ENV === 'production') {
     // 生产环境
-    BASE_URL = 'https://cn.uchat.com.cn/test/gateway'
+    // BASE_URL = 'https://cn.uchat.com.cn/test/gateway'
     // BASE_URL = 'http://192.168.69.130:8888'
-    // BASE_URL = 'https://cn.uchat.com.cn/gateway'
+    BASE_URL = 'https://cn.uchat.com.cn/gateway'
   } else {
     // 测试环境
     BASE_URL = 'https://cn.uchat.com.cn/test/gateway'
